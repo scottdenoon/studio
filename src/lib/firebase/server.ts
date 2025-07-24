039d65c8157304b6ef912150254e54ddc6906061
Scott <scottdenoon@gmail.com>
2025-07-24 02:31:13 +0000

    make sure the app can connect to the firebase database

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -4,13 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "market-momentum-xm3tl",
-  "appId": "1:238114226305:web:a3df500e4077af1759cd80",
-  "storageBucket": "market-momentum-xm3tl.firebasestorage.app",
-  "apiKey": "AIzaSyAFlClLEI2vML6kauzAZxbDVi_ArWIZd6s",
-  "authDomain": "market-momentum-xm3tl.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "238114226305"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase for SSR
